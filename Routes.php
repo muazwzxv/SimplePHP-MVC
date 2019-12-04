2a6acea2e0521b3f7f761726f54823d839bdaeab
Dolpheyn <farissufyan99@gmail.com>
2019-12-04 14:05:50 +0800

    Removed extra endpoints

--- Routes.php
+++ Routes.php
@@ -1,27 +1,17 @@
 
 <?php
 $postData = $_POST;
+
 Route::get('home',function(){
-    Home::renderView();
+    HomeController::renderView();
 });
 
-Route::get('payment',function(){
-	Payment::renderView();
-});
-
-Route::get('Feedback',function(){
-	Feedback::renderView();
-});
 
 Route::get('login', function(){
-	Login::renderView();
-});
-
-Route::get('homeprofile', function(){
-	Profile::renderView();
+	LoginController::renderView();
 });
 
 Route::post('login', function() use ($postData){
-	Login::processLogin($postData);
+	LoginController::processLogin($postData);
 });
 ?>

581803589778178f5b5138f33ab1e355153305c4
Dolpheyn <farissufyan99@gmail.com>
2019-12-04 14:06:40 +0800

    Rewrite autoloader for classes/ to Core/

--- autoloader.php
+++ autoloader.php
@@ -1,7 +1,7 @@
 <?php
     function __autoload($class_name){
-        if(file_exists('classes/'.$class_name.'.php')){
-            require_once 'classes/'.$class_name.'.php';
+        if(file_exists('Core/'.$class_name.'.php')){
+            require_once 'Core/'.$class_name.'.php';
         }
         else if(file_exists('Controller/'.$class_name.'.php')){
 	    require_once 'Controller/'.$class_name.'.php';

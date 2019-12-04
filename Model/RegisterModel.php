30a861926b6da74710071d13abb0fe47570d1c61
Dolpheyn <farissufyan99@gmail.com>
2019-12-04 15:45:40 +0800

    Fixed add curly braces for if statement

--- Model/RegisterModel.php
+++ Model/RegisterModel.php
@@ -4,12 +4,15 @@ class RegisterModel extends Model{
 
 	public function register($username, $email, $phone, $password){
 
-		if(self::existRows(self::query("SELECT * FROM `users` WHERE `username`='$username'")))
+		if(self::existRows(self::query("SELECT * FROM `users` WHERE `username`='$username'"))){
 			// User already exist.
 			die("User already exists");
+		}
 
 		self::query("INSERT INTO `users` (`username`, `email`, `phoneNo`, `password`) 
-			     VALUES ('$username', '$email', '$phone', '$password')";
+			     VALUES ('$username', '$email', '$phone', '$password')");
+
+		//header("Location: http://localhost:80/profile/$username");
     } 
 }
 ?>
